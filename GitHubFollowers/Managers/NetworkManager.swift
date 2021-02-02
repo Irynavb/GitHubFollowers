26c5623cb73ff5269c59c5ea18acf40246ac8a46
Irynavb <irynabetancourt@gmail.com>
2021-02-02 09:53:43 -0500

    cache of the avatars image view

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Iryna V Betancourt on 2/1/21.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL  = "https://api.github.com/users/"
+    private let baseURL  = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() {}
 
